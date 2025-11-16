bc81a698c968a06c3b3118a4d71b6c2e66b18c34
Systemized <VincentNguyen.XLV@gmail.com>
2025-11-16 00:35:07 -0500

    created authService, authContext, and protectedRoutes

---
+++ client/src/services/AuthService.ts
@@ -0,0 +1,71 @@
+const apiURL = "http://localhost:3000";
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    role: string;
+}
+
+export interface AuthResponse {
+    message: string;
+    token: string;
+    user: User;
+}
+
+
+export const authService = {
+    async signUp(name: string, email: string, password: string): Promise<AuthResponse> {
+        const response = await fetch(`${apiURL}/auth/signup`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name, email, password })
+        });
+
+        if (!response.ok) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('user', JSON.stringify(data.user));
+        return data;
+    },
+
+    async login(email: string, password: string): Promise<AuthResponse> {
+        const response = await fetch(`${apiURL}/auth/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+
+        if (!response.ok) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+        localStorage.setitem('user', JSON.stringify(data.user));
+        return data;
+    },
+
+    logout() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+    },
+
+    getToken(): string | null {
+        return localStorage.getItem('token');
+    },
+
+    getUser(): User | null {
+        const userStr = localStorage.getItem('token');
+        return userStr ? JSON.parse(userStr) : null
+    },
+
+    isAuthenticated(): boolean {
+        return !!this.getToken();
+    }
+}
